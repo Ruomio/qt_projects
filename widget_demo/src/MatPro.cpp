ea9444f941608a4e40b46c3c328143e739468b2c
PapillonAz <1065940593@qq.com>
2023-04-01 14:54:09 +0800

    4.1

--- widget_demo/src/MatPro.cpp
+++ widget_demo/src/MatPro.cpp
@@ -2,7 +2,7 @@
  * @Author: PapillonAz 1065940593@qq.com
  * @Date: 2023-02-17 15:27:16
  * @LastEditors: PapillonAz 1065940593@qq.com
- * @LastEditTime: 2023-04-01 14:29:12
+ * @LastEditTime: 2023-04-01 14:33:53
  * @FilePath: /widget_demo/src/MatPro.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -254,7 +254,7 @@ void MatPro::detectFace(cv::Mat mat,cv::Mat afterProcess, int predic, double alp
     
     // 定位出错误。
     try{
-        faceCascade.detectMultiScale(afterProcess, faces, 1.2, 5, 0, Size(30, 30));
+        faceCascade.detectMultiScale(afterProcess, faces, 1.2, 1, 0, Size(30, 30));
     }catch(Exception *e){
         cout<<"detectMultiScale is faile"<<endl;
     }
