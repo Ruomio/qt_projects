9ad2bf9f2a1370571d768a38b0c9d54822d9e19e
PapillonAz <1065940593@qq.com>
2023-04-01 14:29:13 +0800

    4.1

--- widget_demo/src/MatPro.cpp
+++ widget_demo/src/MatPro.cpp
@@ -2,7 +2,7 @@
  * @Author: PapillonAz 1065940593@qq.com
  * @Date: 2023-02-17 15:27:16
  * @LastEditors: PapillonAz 1065940593@qq.com
- * @LastEditTime: 2023-04-01 13:41:40
+ * @LastEditTime: 2023-04-01 14:29:12
  * @FilePath: /widget_demo/src/MatPro.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -252,7 +252,13 @@ void MatPro::detectFace(cv::Mat mat,cv::Mat afterProcess, int predic, double alp
         }
     }
     
-    faceCascade.detectMultiScale(afterProcess, faces, 1.2, 5, 0, Size(30, 30));
+    // 定位出错误。
+    try{
+        faceCascade.detectMultiScale(afterProcess, faces, 1.2, 5, 0, Size(30, 30));
+    }catch(Exception *e){
+        cout<<"detectMultiScale is faile"<<endl;
+    }
+ 
     for (auto b : faces) {
 		cout << "输出一张人脸位置：(x,y):" << "(" << b.x << "," << b.y << ") , (width,height):(" << b.width << "," << b.height << ")" << "this is :"<<str<< endl;   
 	}
